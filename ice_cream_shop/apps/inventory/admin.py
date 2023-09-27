ac990512e720e7a35903d73f3d07897434e48beb
Stepan Kornev <kornevstepan169@gmail.com>
2023-09-27 14:36:40 +0700

    The Category model has been created.

--- ice_cream_shop/apps/inventory/admin.py
+++ ice_cream_shop/apps/inventory/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Product, Category
 
 
 class ProductAdmin(admin.ModelAdmin):
-    pass
+    prepopulated_fields = {'slug': ('name', )}
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name', )}
 
 
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
--- ice_cream_shop/apps/inventory/models.py
+++ ice_cream_shop/apps/inventory/models.py
@@ -45,5 +45,15 @@ class Product(models.Model):
         ordering = ('id', )
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Категория')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ('name', )
 
