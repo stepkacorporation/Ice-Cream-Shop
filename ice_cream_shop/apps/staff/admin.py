2c3ecb9f41a1dfd5d96c643e368ca87b39a08924
Stepan Kornev <kornevstepan169@gmail.com>
2023-10-02 15:51:46 +0700

    Models have been created for the staff application.

--- ice_cream_shop/apps/staff/admin.py
+++ ice_cream_shop/apps/staff/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Employee, Position, Task
+
+
+admin.site.register(Employee)
+admin.site.register(Position)
+admin.site.register(Task)
--- ice_cream_shop/apps/staff/models.py
+++ ice_cream_shop/apps/staff/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
-# Create your models here.
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=255, verbose_name='Имя')
+    last_name = models.CharField(max_length=255, verbose_name='Фамилия')
+    position = models.ForeignKey('Position', on_delete=models.PROTECT, verbose_name='Должность')
+    phone = models.CharField(max_length=15, verbose_name='Телефон')
+    email = models.EmailField(max_length=255, verbose_name='Эл. почта')
+    hire_date = models.DateField(auto_now_add=True, verbose_name='Дата приема на работу')
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} - {self.position}'
+
+    class Meta:
+        verbose_name = 'Работник'
+        verbose_name_plural = 'Работники'
+        ordering = ('last_name', )
+
+
+class Position(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    salary = models.PositiveIntegerField(verbose_name='Зарплата (руб.)')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Должность'
+        verbose_name_plural = 'Должности'
+        ordering = ('title', )
+
+
+class Task(models.Model):
+    TASK_STATUS = (
+        ('pending', 'Ожидает выполнения'),
+        ('processing', 'В процессе'),
+        ('completed', 'Выполнена'),
+    )
+
+    title = models.CharField(max_length=255, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    employee = models.ForeignKey('Employee', on_delete=models.CASCADE, verbose_name='Работник')
+    due_date = models.DateField(verbose_name='Дата выполнения')
+    status = models.CharField(max_length=15, choices=TASK_STATUS, default='pending', verbose_name='Статус')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+        ordering = ('-due_date', )
