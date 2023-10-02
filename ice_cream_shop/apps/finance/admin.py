26e36b976252212187aedcb1686aedd6912fa291
Stepan Kornev <kornevstepan169@gmail.com>
2023-10-02 16:18:56 +0700

    Models have been created for the finance application.

--- ice_cream_shop/apps/finance/admin.py
+++ ice_cream_shop/apps/finance/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Income, Expense
+
+
+admin.site.register(Income)
+admin.site.register(Expense)
--- ice_cream_shop/apps/finance/models.py
+++ ice_cream_shop/apps/finance/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Income(models.Model):
+    amount = models.PositiveIntegerField(verbose_name='Сумма  дохода (руб.)')
+    description = models.TextField(verbose_name='Описание')
+    date = models.DateField(auto_now_add=True, verbose_name='Дата дохода')
+
+    def __str__(self):
+        return f'{self.amount} руб. {self.date}'
+
+    class Meta:
+        verbose_name = 'Доход'
+        verbose_name_plural = 'Доходы'
+        ordering = ('-date',)
+
+
+class Expense(models.Model):
+    amount = models.PositiveIntegerField(verbose_name='Сумма  расхода (руб.)')
+    description = models.TextField(verbose_name='Описание')
+    date = models.DateField(auto_now_add=True, verbose_name='Дата расхода')
+
+    def __str__(self):
+        return f'{self.amount} руб. {self.date}'
+
+    class Meta:
+        verbose_name = 'Расход'
+        verbose_name_plural = 'Расходы'
+        ordering = ('-date', )
+
